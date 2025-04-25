9c9e18c5fa99100f5c10f6a4827664cdded8c8f4
Writingsomelines <123816986+Writingsomelines@users.noreply.github.com>
2025-04-25 17:35:38 +0100

    Update server.py

--- server.py
+++ server.py
@@ -74,4 +74,5 @@ def get_data():
     }), 200
 
 if __name__ == '__main__':
-    app.run(host='100.87.154.176', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
